bdb95b2165154c8e79b7c2dfed377648f29f726d
Mu-munn <rpgmukai333@gmail.com>
2025-05-28 23:18:21 +0900

    update: downgrade @opentelemetry/api to 1.4.1 and update dependencies in pnpm-lock.yaml; add important flag in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	important: true,
     darkMode: ["class"],
     content: [
 		"./app/**/*.{js,ts,jsx,tsx}",
